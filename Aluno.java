281dd627c15536d910dff391ffc73b54622850b2
Marcelo Santos <marcelosantos@great.ufc.br>
2013-07-31 11:02:41 -0300

    Implementing Aluno class on branch marcelo_issue

---
+++ Aluno.java
@@ -0,0 +1,22 @@
+/**
+* Declaração de classe Aluno que herda de Pessoa
+*/
+public class Aluno extends Pessoa
+{
+	
+	//Declaração de atributos
+	private String matricula;
+	
+	//Métodos de acesso
+	public String getMatricula()
+	{
+		return matricula;
+	}	
+
+	//Métodos de modificação
+	public void setMatricula()
+	{
+		this.matricula = matricula;
+	}
+
+}
