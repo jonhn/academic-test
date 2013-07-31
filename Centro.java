d6171b886a2bde15fb8ed89006033c7189dd7d20
Fabiano Freitas <fabianofreitas@great.ufc.br>
2013-07-31 10:32:16 -0300

    Updated Centro, Curso, GradeCurricular, Disciplina class's

--- Centro.java
+++ Centro.java
@@ -1,9 +1,23 @@
 public class Centro {
 	int codCentro;
 	String nome;
+	List<Curso> listCursos;
 
 	public Centro(int codCentro, String nome) {
 		this.codCentro = codCentro;
 		this.nome = nome;
+		this.listCursos = new ArrayList<Curso>();
+	}
+
+	public void addCurso(Curso c) {
+	
+	}
+
+	public void remCurso(Curso c) {
+
+	}
+
+	public Curso getCurso(int codCurso) {
+		return null;
 	}
 }
